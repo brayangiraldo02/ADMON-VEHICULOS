25824b9212de5a7b34760c37696b84bc203401a6
Hector <h.vanegas@utp.edu.co>
2024-04-23 21:23:50 -0500

    add routes

--- ProjectPanamaBackend/routes/reports.py
+++ ProjectPanamaBackend/routes/reports.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 from config.dbconnection import session
 from models.estados import Estados
 from models.vehiculos import Vehiculos
+from models.propietarios import Propietarios
 from fastapi.encoders import jsonable_encoder
 
 reports_router = APIRouter()
@@ -18,3 +19,9 @@ async def get_vehiculos():
   db = session()
   vehiculos = db.query(Vehiculos).all()
   return JSONResponse(content=jsonable_encoder(vehiculos))
+
+@reports_router.get('/propietarios')
+async def get_propietarios():
+  db = session()
+  propietarios = db.query(Propietarios).all()
+  return JSONResponse(content=jsonable_encoder(propietarios))
