3f614fae61ce3343a8ac155d76a6708070b945e8
Brayan Giraldo <b.catano@utp.edu.co>
2024-04-27 18:59:53 -0500

    Add home component

--- ProjectPanamaFrontEnd/NewProjectPanamaFrontEnd/src/app/app.module.ts
+++ ProjectPanamaFrontEnd/NewProjectPanamaFrontEnd/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/main/header/header.component';
+import { HomeComponent } from './components/main/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
