2191046fb4ce053da68a3c73d6003288b6c508cc
Brayan Giraldo <b.catano@utp.edu.co>
2024-05-14 23:39:03 -0500

    Add statevehiclefleet view

--- ProjectPanamaFrontEnd/NewProjectPanamaFrontEnd/src/app/components/tasks/statevehiclefleet/statevehiclefleet.component.ts
+++ ProjectPanamaFrontEnd/NewProjectPanamaFrontEnd/src/app/components/tasks/statevehiclefleet/statevehiclefleet.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-statevehiclefleet',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class StatevehiclefleetComponent {
 
+  constructor(private router: Router) { }
+
+  home(): void {
+    this.router.navigate(['/home']);
+  }
 }
