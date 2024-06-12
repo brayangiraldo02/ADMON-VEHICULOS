1096372ea7d33fba631c1139744013895e0fe85d
Hector <h.vanegas@utp.edu.co>
2024-06-12 17:36:50 -0500

    add new endpont

--- ProjectPanamaBackend/routes/reports.py
+++ ProjectPanamaBackend/routes/reports.py
@@ -124,7 +124,7 @@ async def get_conteo_propietarios_vehiculos_estados(id: int):
     try:
         conteo_propietarios_vehiculos_estados = db.query(
             Propietarios.CODIGO.label('propietario_codigo'), 
-            Propietarios.ABREVIADO.label('propietario_abreviado'), 
+            Propietarios.NOMBRE.label('propietario_nombre'), 
             Estados.CODIGO.label('estado_codigo'),
             Estados.NOMBRE.label('estado_nombre'), 
             Vehiculos.NUMERO.label('vehiculo_numero')
@@ -138,7 +138,7 @@ async def get_conteo_propietarios_vehiculos_estados(id: int):
         for resultado in conteo_propietarios_vehiculos_estados:
             propietarios_vehiculos_estados = {
                 'propietario_codigo': resultado.propietario_codigo,
-                'propietario_abreviado': resultado.propietario_abreviado,
+                'propietario_nombre': resultado.propietario_nombre,
                 'estado_codigo': resultado.estado_codigo,
                 'estado_nombre': resultado.estado_nombre,
                 'vehiculo_numero': resultado.vehiculo_numero
@@ -238,7 +238,7 @@ async def get_conteo_propietarios_vehiculos_estados(id: int):
 #-----------------------------------------------------------------------------------------
 
 
-@reports_router.get('/conteo-vehiculos-estados-numeros', response_class=FileResponse)
+@reports_router.get('/informe-estados-detallado', response_class=FileResponse)
 async def get_conteo_vehiculos_estados_numeros():
   db = session()
   try:
@@ -373,13 +373,13 @@ async def get_conteo_vehiculos_estados_numeros():
 
 # -----------------------------------------------------------------------------------------
 
-@reports_router.get('/conteo-propietarios-vehiculos-estados-numeros/{id}', response_class=FileResponse)
+@reports_router.get('/informe-estados-detallado-empresa/{id}', response_class=FileResponse)
 async def get_conteo_propietarios_vehiculos_estados_numeros(id: int):
   db = session()
   try:
     conteo_propietarios_vehiculos_estados = db.query(
         Propietarios.CODIGO.label('propietario_codigo'),
-        Propietarios.ABREVIADO.label('propietario_abreviado'),
+        Propietarios.NOMBRE.label('propietario_nombre'),
         Estados.CODIGO.label('estado_codigo'),
         Estados.NOMBRE.label('estado_nombre'),
         Vehiculos.NUMERO.label('vehiculo_numero')
@@ -389,7 +389,7 @@ async def get_conteo_propietarios_vehiculos_estados_numeros(id: int):
     for resultado in conteo_propietarios_vehiculos_estados:
         propietarios_vehiculos_estados = {
             'propietario_codigo': resultado.propietario_codigo,
-            'propietario_abreviado': resultado.propietario_abreviado,
+            'propietario_nombre': resultado.propietario_nombre,
             'estado_codigo': resultado.estado_codigo,
             'estado_nombre': resultado.estado_nombre,
             'vehiculo_numero': resultado.vehiculo_numero
@@ -519,7 +519,7 @@ async def get_vehiculos_detalles():
     try:
         vehiculos_detalles = db.query(
             Propietarios.CODIGO.label('propietario_codigo'),
-            Propietarios.ABREVIADO.label('propietario_abreviado'),
+            Propietarios.NOMBRE.label('propietario_nombre'),
             Estados.CODIGO.label('estado_codigo'),
             Estados.NOMBRE.label('estado_nombre'),
             Vehiculos.NUMERO.label('vehiculo_numero'),
@@ -547,7 +547,7 @@ async def get_vehiculos_detalles():
         for resultado in vehiculos_detalles:
             vehiculo_detalle = {
                 'propietario_codigo': resultado.propietario_codigo,
-                'propietario_abreviado': resultado.propietario_abreviado,
+                'propietario_nombre': resultado.propietario_nombre,
                 'estado_codigo': resultado.estado_codigo,
                 'estado_nombre': resultado.estado_nombre,
                 'vehiculo_numero': resultado.vehiculo_numero,
@@ -594,7 +594,7 @@ async def get_vehiculos_detalles():
                 # Agregar información de la empresa
                 data_view[empresa] = {
                     "codigo_empresa": info.get("propietario_codigo", 0),
-                    "nombre_empresa": info.get("propietario_abreviado", 0),
+                    "nombre_empresa": info.get("propietario_nombre", 0),
                     "empty": info.get("empty", 0),
                     "estados": {}  # Aquí almacenaremos los vehículos organizados por estado
                 }
@@ -676,7 +676,7 @@ async def get_vehiculos_detalles():
     try:
         vehiculos_detalles = db.query(
             Propietarios.CODIGO.label('propietario_codigo'),
-            Propietarios.ABREVIADO.label('propietario_abreviado'),
+            Propietarios.NOMBRE.label('propietario_nombre'),
             Conductores.CODIGO.label('conductor_codigo'),
             Conductores.NOMBRE.label('conductor_nombre'),
             Conductores.FEC_INGRES.label('conductor_fecha_ingreso'),
@@ -698,7 +698,7 @@ async def get_vehiculos_detalles():
         for resultado in vehiculos_detalles:
             vehiculo_detalle = {
                 'propietario_codigo': resultado.propietario_codigo,
-                'propietario_abreviado': resultado.propietario_abreviado,
+                'propietario_nombre': resultado.propietario_nombre,
                 'conductor_codigo': resultado.conductor_codigo,
                 'conductor_nombre': resultado.conductor_nombre,
                 'conductor_fecha_ingreso': resultado.conductor_fecha_ingreso,
@@ -740,7 +740,7 @@ async def get_vehiculos_detalles():
             if isinstance(info, dict):
                 data_view[empresa] = {
                     "codigo_empresa": info.get("propietario_codigo", 0),
-                    "nombre_empresa": info.get("propietario_abreviado", 0),
+                    "nombre_empresa": info.get("propietario_nombre", 0),
                     "empty": info.get("empty", 0)
                 }
                 consecutivo = 1
@@ -804,5 +804,125 @@ async def get_vehiculos_detalles():
         return response
 
         #return JSONResponse(content=jsonable_encoder(datos))
+    finally:
+        db.close()
+
+#-----------------------------------------------------------------------------------------
+
+@reports_router.get('/directorio-propietarios')
+async def get_propietarios_detalles():
+    db = session()
+    try:
+        propietarios_detalles = db.query(
+            Propietarios.ESTADO.label('propietario_estado'),
+            Propietarios.CODIGO.label('propietario_codigo'),
+            Propietarios.NOMBRE.label('propietario_nombre'),
+            Propietarios.CIUDAD.label('propietario_ciudad'),
+            Propietarios.DIRECCION.label('propietario_direccion'),
+            Propietarios.TELEFONO.label('propietario_telefono'),
+            Propietarios.CELULAR.label('propietario_celular'),
+            Propietarios.CORREO.label('propietario_correo'),
+        )  .all()
+        
+        # Convertir los resultados en un formato JSON
+        propietarios_detalles_list = []
+        for resultado in propietarios_detalles:
+            propietario_detalle = {
+                'propietario_estado': resultado.propietario_estado,
+                'propietario_codigo': resultado.propietario_codigo,
+                'propietario_nombre': resultado.propietario_nombre,
+                'propietario_ciudad': resultado.propietario_ciudad,
+                'propietario_direccion': resultado.propietario_direccion,
+                'propietario_telefono': resultado.propietario_telefono,
+                'propietario_celular': resultado.propietario_celular,
+                'propietario_correo': resultado.propietario_correo
+            }
+            propietarios_detalles_list.append(propietario_detalle)
+    
+        #data = cuotas_pagas(propietarios_detalles_list)
+        data = propietarios_detalles_list
+
+        """ # Datos de la fecha y hora actual
+        fecha = datetime.now().strftime("%Y-%m-%d")
+        hora_actual = datetime.now().strftime("%H:%M:%S")
+        usuario = "admin" 
+        titulo = 'Informe Cuotas Pagas de conductores'
+
+        # Inicializar el diccionario data_view con información común
+        data_view = {
+            "fecha": fecha,
+            "hora": hora_actual
+        }
+
+        # Iterar sobre las empresas y vehículos
+        total = 0
+        for empresa, info in data.items(): 
+            if isinstance(info, dict):
+                data_view[empresa] = {
+                    "codigo_empresa": info.get("propietario_codigo", 0),
+                    "nombre_empresa": info.get("propietario_nombre", 0),
+                    "empty": info.get("empty", 0)
+                }
+                consecutivo = 1
+                for vehiculo, vehiculo_info in info.items():
+                    if isinstance(vehiculo_info, dict):
+                        data_view[vehiculo] = {
+                            "consecutivo": consecutivo,
+                            "codigo_empresa": info.get("propietario_codigo", 0),
+                            "codigo_conductor": vehiculo_info.get("conductor_codigo", 0),
+                            "nombre": vehiculo_info.get("conductor_nombre", 0),
+                            "fecha_ingreso": vehiculo_info.get("conductor_fecha_ingreso", 0),
+                            "numero": vehiculo_info.get("vehiculo_numero", 0),
+                            "prestado": vehiculo_info.get("conductor_und_pre", 0),
+                            "estado": vehiculo_info.get("estado_nombre", 0),
+                            "valor": vehiculo_info.get("conductor_vlr_cuo_diaria", 0),
+                            "num_cuotas": vehiculo_info.get("conductor_nro_cuotas", 0),
+                            "num_cuotas_pagas": vehiculo_info.get("conductor_nro_cuotas_pagas", 0),
+                            "num_cuotas_pendientes": vehiculo_info.get("conductor_nro_ent_sdo", 0),
+                        }
+                        consecutivo += 1
+                        total += 1
+                    else:
+                        pass
+        
+        data_view["total"] = total
+        data_view["usuario"] = usuario
+
+        headers = {
+            "Content-Disposition": "inline; informe-cuotas-pagas.pdf"
+        }  
+
+        template_loader = jinja2.FileSystemLoader(searchpath="./templates")
+        template_env = jinja2.Environment(loader=template_loader)
+        template_file = "InformeCuotas.html"
+        header_file = "header.html"
+        footer_file = "footer.html"
+        template = template_env.get_template(template_file)
+        header = template_env.get_template(header_file)
+        footer = template_env.get_template(footer_file)
+        output_text = template.render(data_view=data_view)
+        output_header = header.render(data_view=data_view)
+        output_footer = footer.render(data_view=data_view)
+
+        html_path = f'./templates/renderInformeCuotas.html'
+        header_path = f'./templates/renderheader.html'
+        footer_path = f'./templates/renderfooter.html'
+        html_file = open(html_path, 'w')
+        header_file = open(header_path, 'w')
+        html_footer = open(footer_path, 'w') 
+        html_file.write(output_text)
+        header_file.write(output_header)
+        html_footer.write(output_footer) 
+        html_file.close()
+        header_file.close()
+        html_footer.close()
+        pdf_path = 'informe-cuotas-pagas.pdf'
+        html2pdf(titulo, html_path, pdf_path, header_path=header_path, footer_path=footer_path)
+
+        response = FileResponse(pdf_path, media_type='application/pdf', filename='templates/informe-cuotas-pagas.pdf', headers=headers) """
+        
+        #return response
+
+        return JSONResponse(content=jsonable_encoder(data))
     finally:
         db.close()
