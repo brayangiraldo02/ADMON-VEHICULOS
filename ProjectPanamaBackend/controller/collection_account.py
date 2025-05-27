fba7b391d3c16f5c960d131007034fb9b6bf3795
xbedoya <ximena.bedoya@utp.edu.co>
2025-05-26 19:45:46 -0500

    fix(backend-collectionAccounts): invalid data correction

--- ProjectPanamaBackend/controller/collection_account.py
+++ ProjectPanamaBackend/controller/collection_account.py
@@ -13,6 +13,9 @@ from collections import defaultdict
 async def get_collection_accounts(companies_list: list):
   db = session()
   try:
+    print(f"Companies List: {companies_list[0]}")
+    company_code = db.query(Propietarios.EMPRESA).filter(Propietarios.CODIGO == companies_list[0])
+
     collectionAccounts = db.query(
                           Propietarios.CODIGO.label('propietarios_codigo'), 
                           Propietarios.NOMBRE.label('propietarios_nombre'), 
@@ -25,8 +28,7 @@ async def get_collection_accounts(companies_list: list):
                           Centrales.NOMBRE.label('centrales_nombre'), 
                           Conductores.CODIGO.label('conductores_codigo'),
                           Conductores.NOMBRE.label('conductores_nombre'), 
-                          Conductores.CEDULA.label('conductores_cedula'), 
-                          Conductores.TELEFONO.label('conductores_telefono'), 
+                          Conductores.CEDULA.label('conductores_cedula'),  
                           Conductores.CELULAR.label('conductores_celular'), 
                           Conductores.FEC_INGRES.label('conductores_fecingres'), 
                           Conductores.CUO_DIARIA.label('conductores_cuodiaria')
@@ -41,13 +43,14 @@ async def get_collection_accounts(companies_list: list):
                         ).filter(
                           Vehiculos.PROPI_IDEN.in_(companies_list), 
                           Vehiculos.ESTADO.in_(['01', '19']), 
-                          Vehiculos.CONDUCTOR != ''
+                          Vehiculos.CONDUCTOR != '',
+                          Centrales.EMPRESA == company_code.scalar_subquery()
                         ).all()
     
     if not collectionAccounts:
       return JSONResponse(content=jsonable_encoder({'message': 'No collection accounts found'}), status_code=404)
     
-    conductor_placa_set = {(collectionAccount.conductores_codigo, collectionAccount.vehiculos_placa) for collectionAccount in collectionAccounts}
+    conductor_placa_set = {(collectionAccount.conductores_codigo, collectionAccount.vehiculos_numero) for collectionAccount in collectionAccounts}
 
     cartera_data = db.query(
                       Cartera.CLIENTE,
@@ -70,13 +73,17 @@ async def get_collection_accounts(companies_list: list):
         saldos_dict[(row.CLIENTE, row.UNIDAD, row.TIPO)] = row.SALDO
 
     collectionAccounts_list = []
+    collectionAccount_prev = None
 
     for collectionAccount in collectionAccounts:
-      key_base = (collectionAccount.conductores_codigo, collectionAccount.vehiculos_placa)
+      key_base = (collectionAccount.conductores_codigo, collectionAccount.vehiculos_numero)
       deu_renta = saldos_dict.get((*key_base, '10'), 0)
       fon_inscri = saldos_dict.get((*key_base, '01'), 0)
       deu_sinies = saldos_dict.get((*key_base, '11'), 0)
       deu_otras = sum(saldo for (cli, uni, tipo), saldo in saldos_dict.items() if (cli, uni) == key_base and tipo > '11')
+
+      if deu_renta == 0:
+        continue
       
       collectionAccount_temp = {
         'codigo': collectionAccount.propietarios_codigo, 
@@ -88,18 +95,21 @@ async def get_collection_accounts(companies_list: list):
         'centrales_nombre': collectionAccount.centrales_nombre, 
         'conductores_nombre': collectionAccount.conductores_nombre, 
         'conductores_cedula': collectionAccount.conductores_cedula, 
-        'conductores_telefono': collectionAccount.conductores_telefono, 
         'conductores_celular': collectionAccount.conductores_celular, 
         'conductores_fecingres': collectionAccount.conductores_fecingres, 
         'conductores_cuodiaria': collectionAccount.conductores_cuodiaria,
         'deu_renta': deu_renta,
         'fon_inscri': fon_inscri,
-        'diferencia': deu_renta - fon_inscri,
+        'diferencia': fon_inscri - deu_renta,
         'deu_sinies': deu_sinies,
         'deu_otras': deu_otras
       }
 
-      collectionAccounts_list.append(collectionAccount_temp)
+      if collectionAccount_prev and (collectionAccount_prev['conductor'] == collectionAccount_temp['conductor'] and collectionAccount_prev['vehiculo_numero'] == collectionAccount_temp['vehiculo_numero']):
+        pass
+      else:
+        collectionAccount_prev = collectionAccount_temp
+        collectionAccounts_list.append(collectionAccount_temp)
 
     return JSONResponse(content=jsonable_encoder(collectionAccounts_list), status_code=200)
   except Exception as e:
