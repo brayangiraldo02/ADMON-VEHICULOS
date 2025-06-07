27e0cd8c4d8d336c05f811f0aaac8dbeef54e7e0
xbedoya <ximena.bedoya@utp.edu.co>
2025-06-06 21:50:01 -0500

    feat(backend-collectionAccounts): add endpoint for downloading collection account pdf

--- ProjectPanamaBackend/utils/pdf.py
+++ ProjectPanamaBackend/utils/pdf.py
@@ -1,5 +1,6 @@
 import pdfkit
 import os
+from io import BytesIO
 
 def html2pdf(titulo, html_path, pdf_path, header_path, footer_path, orientation='Portrait'):
     """
@@ -31,5 +32,10 @@ def html2pdf(titulo, html_path, pdf_path, header_path, footer_path, orientation=
         'orientation': orientation,        
     }
 
-    with open(html_path) as f: 
-        pdfkit.from_file(f, pdf_path, options=options)
+    if isinstance(pdf_path, BytesIO):
+        with open(html_path) as f:
+            pdf_bytes = pdfkit.from_file(f, False, options=options)
+        pdf_path.write(pdf_bytes)
+    elif isinstance(pdf_path, (str, os.PathLike)):
+        with open(html_path) as f: 
+            pdfkit.from_file(f, pdf_path, options=options)
