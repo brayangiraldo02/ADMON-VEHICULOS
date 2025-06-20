d501f4aaea34be9ee980eac63db5095079ff68e3
Brayan Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2025-06-20 09:25:02 -0500

    feat(navigationModule): second preview of the new navigation bar

--- ProjectPanamaFrontEnd/NewProjectPanamaFrontEnd/src/app/modules/navigation/navigation.module.ts
+++ ProjectPanamaFrontEnd/NewProjectPanamaFrontEnd/src/app/modules/navigation/navigation.module.ts
@@ -6,12 +6,17 @@ import { LayoutComponent } from './components/layout/layout.component';
 import { MaterialModule } from '../shared/material/material.module'; 
 import { SharedModule } from '../shared/shared.module';
 import { DevPreviewComponent } from './components/dev-preview/dev-preview.component'; // Assuming you have a SharedModule for common components, directives, etc.
-
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     LayoutComponent,
-    DevPreviewComponent
+    DevPreviewComponent,
+    ToolbarComponent,
+    SidenavComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule,
--- ProjectPanamaFrontEnd/NewProjectPanamaFrontEnd/src/app/modules/shared/shared.module.ts
+++ ProjectPanamaFrontEnd/NewProjectPanamaFrontEnd/src/app/modules/shared/shared.module.ts
@@ -5,17 +5,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { InfoCompanyComponent } from './components/info-company/info-company.component';
 import { FormsModule } from '@angular/forms';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MaterialModule } from './material/material.module';
-import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent,
     InfoCompanyComponent,
-    ToolbarComponent,
-    SidenavComponent,
   ],
   imports: [
     CommonModule,
@@ -25,9 +20,6 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
   ],
   exports: [
     HeaderComponent,
-    FooterComponent,
-    ToolbarComponent,
-    SidenavComponent,
     MaterialModule
   ]
 })
