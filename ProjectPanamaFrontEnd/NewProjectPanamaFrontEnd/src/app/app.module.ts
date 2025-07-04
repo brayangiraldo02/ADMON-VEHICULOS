0689d5e809c169eac8cff0fd505f4d385eb8ca21
Brayan Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2025-07-03 21:31:18 -0500

    fix(frontend): date order component

--- ProjectPanamaFrontEnd/NewProjectPanamaFrontEnd/src/app/app.module.ts
+++ ProjectPanamaFrontEnd/NewProjectPanamaFrontEnd/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -92,7 +92,9 @@ import { InspectionsComponent } from './modules/tasks/inspections/inspections.co
     SharedModule,
     MaterialModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
