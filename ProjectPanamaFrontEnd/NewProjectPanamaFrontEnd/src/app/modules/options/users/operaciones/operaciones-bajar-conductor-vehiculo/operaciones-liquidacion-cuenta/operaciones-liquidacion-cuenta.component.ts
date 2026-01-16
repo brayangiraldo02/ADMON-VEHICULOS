4e54ee70a81e755009cddb9c049e63bfb70dfeea
Brayan Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2026-01-15 23:50:06 -0500

    fix(front-operations): first validations in 'bajar conductor vehiculo' and 'liquidacion cuenta'

--- ProjectPanamaFrontEnd/NewProjectPanamaFrontEnd/src/app/modules/options/users/operaciones/operaciones-bajar-conductor-vehiculo/operaciones-liquidacion-cuenta/operaciones-liquidacion-cuenta.component.ts
+++ ProjectPanamaFrontEnd/NewProjectPanamaFrontEnd/src/app/modules/options/users/operaciones/operaciones-bajar-conductor-vehiculo/operaciones-liquidacion-cuenta/operaciones-liquidacion-cuenta.component.ts
@@ -8,17 +8,26 @@ import {
   OperacionesLiquidacionOtrosGastosComponent,
   OtrosGastosResult,
 } from '../operaciones-otros-gastos/operaciones-liquidacion-otros-gastos/operaciones-liquidacion-otros-gastos.component';
+import { ApiService } from 'src/app/services/api.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpErrorResponse } from '@angular/common/http';
 
 export interface LiquidationDetail {
-  inscripcion: number;
-  rentaDiaria: number;
-  otrasDeudas: number;
+  registration: number;
+  daily_rent: number;
+  accidents: number;
+  other_debts: number;
   panapass: number;
-  totalDeudas: number;
-  siniestros: number;
-  otrosGastos: number;
-  montoDevolver: number;
-  montoDeudar: number;
+  total_debt: number;
+  other_expenses: number;
+  owed_to_driver: number;
+  owed_by_driver: number;
+}
+
+interface dialogData {
+  companyCode: string;
+  vehicleNumber: string;
+  driverNumber: string;
 }
 
 @Component({
@@ -27,104 +36,98 @@ export interface LiquidationDetail {
   styleUrls: ['./operaciones-liquidacion-cuenta.component.css'],
 })
 export class OperacionesLiquidacionCuentaComponent implements OnInit {
-  // Variables de estado
   isLoading: boolean = true;
 
-  // Almacenar los items de otros gastos modificados
+  // TODO: Modificar interface en any.
   otrosGastosItems: any[] = [];
 
-  // Inicializamos la data en null o con valores en 0 para evitar errores en template antes de cargar
   data: LiquidationDetail = {
-    inscripcion: 0,
-    rentaDiaria: 0,
-    otrasDeudas: 0,
+    registration: 0,
+    daily_rent: 0,
+    other_debts: 0,
     panapass: 0,
-    totalDeudas: 0,
-    siniestros: 0,
-    otrosGastos: 0,
-    montoDevolver: 0,
-    montoDeudar: 0,
+    total_debt: 0,
+    accidents: 0,
+    other_expenses: 0,
+    owed_to_driver: 0,
+    owed_by_driver: 0,
   };
 
   constructor(
     public dialogRef: MatDialogRef<OperacionesLiquidacionCuentaComponent>,
-    @Inject(MAT_DIALOG_DATA) public incomingData: any, // Por si pasas ID de conductor, etc.
-    private dialog: MatDialog
+    @Inject(MAT_DIALOG_DATA) public incomingData: dialogData, 
+    private dialog: MatDialog,
+    private apiService: ApiService,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
     this.getLiquidationData();
   }
 
-  // Simulación de petición al Backend
   getLiquidationData(): void {
     this.isLoading = true;
 
-    // Simulamos un delay de 1.5 segundos
-    setTimeout(() => {
-      // AQUÍ ESTÁN TUS DATOS QUEMADOS
-      // Estos valores coinciden con la lógica: Fondos - Deudas = Devolver
-      this.data = {
-        inscripcion: 200.0, // Fondo
-        rentaDiaria: 72.0, // Deuda
-        otrasDeudas: 10.5, // Deuda
-        panapass: 5.5, // Deuda
-        siniestros: 20.0, // Deuda
-        // Totales calculados (simulados)
-        totalDeudas: 88.0, // 72 + 10.50 + 5.50
-        otrosGastos: 0.0, // Input disabled
-
-        // Resultados
-        montoDevolver: 112.0, // 200 - 88
-        montoDeudar: 0.0,
-      };
-
+    if(this.incomingData.companyCode === '' || this.incomingData.vehicleNumber === '' || this.incomingData.driverNumber === '') {
       this.isLoading = false;
-    }, 1500);
+      this.openSnackbar('Error al obtener la información de la liquidación.');
+      return;
+    }
+
+    this.apiService.getData(`operations/remove-driver/${this.incomingData.companyCode}/${this.incomingData.vehicleNumber}/${this.incomingData.driverNumber}`).subscribe({
+      next: (data: LiquidationDetail) => {
+        this.data = data;
+        this.isLoading = false;
+      },
+      error: (error: HttpErrorResponse) => {
+        this.isLoading = false;
+        this.openSnackbar('Error al obtener la información de la liquidación.');
+      },
+    });
   }
 
   // Acción para el botón del "ojo" o "más" en Otros Gastos
   openOtrosGastos(): void {
-    const dialogRef = this.dialog.open(
-      OperacionesLiquidacionOtrosGastosComponent,
-      {
-        width: '850px',
-        maxWidth: '95vw',
-        maxHeight: '85vh',
-        data: {
-          gastosItems: this.otrosGastosItems, // Pasar los items guardados para persistir
-        },
-      }
-    );
+    // const dialogRef = this.dialog.open(
+    //   OperacionesLiquidacionOtrosGastosComponent,
+    //   {
+    //     width: '850px',
+    //     maxWidth: '95vw',
+    //     maxHeight: '85vh',
+    //     data: {
+    //       gastosItems: this.otrosGastosItems, // Pasar los items guardados para persistir
+    //     },
+    //   }
+    // );
 
-    dialogRef.afterClosed().subscribe((result: OtrosGastosResult | null) => {
-      if (result) {
-        // Guardar los items modificados
-        this.otrosGastosItems = result.items;
+    // dialogRef.afterClosed().subscribe((result: OtrosGastosResult | null) => {
+    //   if (result) {
+    //     // Guardar los items modificados
+    //     this.otrosGastosItems = result.items;
 
-        // Actualizar el valor de otros gastos
-        this.data.otrosGastos = result.totalOtrosGastos;
+    //     // Actualizar el valor de otros gastos
+    //     this.data.otrosGastos = result.totalOtrosGastos;
 
-        // Recalcular los montos
-        this.recalcularMontos();
+    //     // Recalcular los montos
+    //     this.recalcularMontos();
 
-        console.log('Otros gastos actualizados:', result);
-      }
-    });
+    //     console.log('Otros gastos actualizados:', result);
+    //   }
+    // });
   }
 
   // Recalcula los montos después de actualizar otros gastos
   private recalcularMontos(): void {
-    const totalFondos = this.data.inscripcion;
-    const totalDeudas = this.data.totalDeudas + this.data.otrosGastos;
+    // const totalFondos = this.data.inscripcion;
+    // const totalDeudas = this.data.totalDeudas + this.data.otrosGastos;
 
-    if (totalFondos >= totalDeudas) {
-      this.data.montoDevolver = totalFondos - totalDeudas;
-      this.data.montoDeudar = 0;
-    } else {
-      this.data.montoDevolver = 0;
-      this.data.montoDeudar = totalDeudas - totalFondos;
-    }
+    // if (totalFondos >= totalDeudas) {
+    //   this.data.montoDevolver = totalFondos - totalDeudas;
+    //   this.data.montoDeudar = 0;
+    // } else {
+    //   this.data.montoDevolver = 0;
+    //   this.data.montoDeudar = totalDeudas - totalFondos;
+    // }
   }
 
   // Acción de Imprimir
@@ -142,5 +145,13 @@ export class OperacionesLiquidacionCuentaComponent implements OnInit {
       data: this.data,
       otrosGastosItems: this.otrosGastosItems,
     });
+  }
+
+  openSnackbar(message: string) {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    })
   }
 }
